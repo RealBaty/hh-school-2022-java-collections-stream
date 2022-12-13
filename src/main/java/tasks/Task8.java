54b8d51ed81d0179339c3b92f21b0f53fb1c4e03
RealBaty <realbaty@gmail.com>
2022-12-13 23:21:24 +0300

    Добавлены комментарии в задании 8

--- src/main/java/tasks/Task8.java
+++ src/main/java/tasks/Task8.java
@@ -18,6 +18,10 @@ P.P.S Здесь ваши правки желательно прокоммент
 public class Task8 {
 
   //Не хотим выдавать апи нашу фальшивую персону, поэтому конвертим начиная со второй
+  /*
+  - Пропуск первой персоны реализован в stream, это позволило улучшить читаемость кода,
+  а так же избавляет от необходимости проверять длину списка.
+   */
   public List<String> getNames(List<Person> persons) {
     return persons.stream()
             .skip(1)
@@ -26,6 +30,10 @@ public class Task8 {
   }
 
   //ну и различные имена тоже хочется
+  /*
+  - Не используется функция getNames, что позволяет повторно не раскладывать список в stream
+  - В операторе distinct нет необходимости, в итоге в множестве все элементы уникальны
+   */
   public Set<String> getDifferentNames(List<Person> persons) {
     return persons.stream()
             .skip(1)
@@ -34,13 +42,21 @@ public class Task8 {
   }
 
   //Для фронтов выдадим полное имя, а то сами не могут
+  /*
+  - Добавлено отчество, вместо второй фамилии. Более читаемая реализация через stream
+   */
   public String convertPersonToString(Person person) {
     return Stream.of(person.getFirstName(), person.getMiddleName(), person.getSecondName())
             .filter(Objects::nonNull)
             .collect(Collectors.joining(" "));
   }
 
-  // словарь id персоны -> ее имя (возможно полное имя) (нужно ли скипать первую персону?)
+  // словарь id персоны -> ее имя
+  /*
+  - При создании HashMap не указывается её размер, размер 1 сразу приведет к расширению хэш таблицы
+  - Не нужно проверять наличие ключа, перед тем как положить значение в map
+  - Переименованы переменные на более читаемые
+   */
   public Map<Integer, String> getPersonNames(Collection<Person> persons) {
     Map<Integer, String> nameByIdPersons = new HashMap<>();
     for(var person: persons){
@@ -50,6 +66,10 @@ public class Task8 {
   }
 
   // есть ли совпадающие в двух коллекциях персоны?
+  /*
+  - Более быстрая реализация, работающая за O(n + m) вместо O(n * m)
+  - Улучшена читаемость за счет отсутствия вложенных циклов
+   */
   public boolean hasSamePersons(Collection<Person> persons1, Collection<Person> persons2) {
     Set<Person> personsSet1 = new HashSet<>(persons1);
     for(var person2: persons2){
@@ -61,6 +81,10 @@ public class Task8 {
   }
 
   //...
+  /*
+  - Убрано поле класса count, т.к. оно приватное и ни где больше не использовалось
+  - Подсчет реализован через count() в угоду читаемости
+   */
   public long countEven(Stream<Integer> numbers) {
     return numbers
             .filter(num -> num % 2 == 0)
